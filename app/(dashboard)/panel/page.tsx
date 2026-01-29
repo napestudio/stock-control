b1e9006ec29f28d95e49f7cbfb3b902e1fc8303a
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-01-28 22:48:14 -0300

    First push

---
+++ app/(dashboard)/panel/page.tsx
@@ -0,0 +1,12 @@
+import DashboardHeader from "@/components/dashboard-header";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <DashboardHeader
+        title="Panel de administración"
+        subtitle="Este es el panel de administración"
+      />
+    </div>
+  );
+}
---
+++ components/dashboard-header.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+interface DashboardHeaderProps {
+  title: string;
+  subtitle?: string;
+}
+
+export default function DashboardHeader({
+  title,
+  subtitle,
+}: DashboardHeaderProps) {
+  return (
+    <header className="flex items-center justify-between">
+      <div>
+        <h1 className="text-2xl font-bold">{title}</h1>
+        {subtitle && <h2>{subtitle}</h2>}
+      </div>
+    </header>
+  );
+}
